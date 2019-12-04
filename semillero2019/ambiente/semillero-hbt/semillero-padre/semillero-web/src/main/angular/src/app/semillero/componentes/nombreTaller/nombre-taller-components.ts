38785969167b89efab39ec8be01ec750385c83a1
diecastil12 <die.delcastillo@outlook.com>
2019-12-04 12:18:13 -0500

    Tuve problemas con actualización del git

---
+++ semillero2019/ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angular/src/app/semillero/componentes/nombreTaller/nombre-taller-components.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+/**
+ * @description Componenente para mostrar nombre del alumno
+ * 
+ * @author Diego Castillo 
+ */
+@Component({
+    selector: 'nombre-taller',
+    templateUrl: './nombre-taller-components.html',
+
+})
+
+export class NombreTallerComponents implements OnInit{
+    ngOnInit() : void{
+
+    }
+
+    constructor (){
+
+    }
+}
